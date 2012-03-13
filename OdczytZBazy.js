b792080cde655b2ba6e6330827b0630645d11485
Daniel <majin.d@gmail.com>
2012-03-13 01:27:05 +0100

    Oczytuje id wpisów w bazie

--- OdczytZBazy.js
+++ OdczytZBazy.js
@@ -1,4 +1,5 @@
 var http = require('http'),
+    jquery = require('jquery'),
     url = require('url');
 
 var options = {
@@ -9,11 +10,15 @@ var options = {
 };
 
 var req = http.request(options, function(res) {
- // console.log('STATUS: ' + res.statusCode);
- // console.log('HEADERS: ' + JSON.stringify(res.headers));
+  //console.log('STATUS: ' + res.statusCode);
+  //console.log('HEADERS: ' + JSON.stringify(res.headers));
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
-    console.log( chunk);
+		chunk = chunk.replace(",",'');
+		chunk = chunk.replace("\n",'');
+		chunk = chunk.replace("{\"id\":\"",'').replace("{[",'').replace("]}",'');
+		var tmp = chunk.split('"');
+    console.log(tmp[0]);
   });
 });
 

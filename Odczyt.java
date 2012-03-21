6924defa26a155438152c243bd0b54c1536b827c
Daniel <majin.d@gmail.com>
2012-03-21 23:46:36 +0100

    prarsowanie do json part 1

--- Odczyt.java
+++ Odczyt.java
@@ -16,14 +16,20 @@ public class Odczyt {
 		URL link = new URL("http://localhost:5984/katastrofy/_all_docs?keys=[%22ID1%22,%22ID2%22]&include_docs=true");
 		URLConnection polaczenie = link.openConnection();
 		BufferedReader response = new BufferedReader(new InputStreamReader(polaczenie.getInputStream()));
-		String inputString;
-		
+		String inputString = "";
+		String inputString2 = "";
+		int i = 0;
+		response.readLine();
 		while ((inputString = response.readLine())!=null) {
-			//System.out.println(inputString);
-			//bufor.write(inputString.toString());
-			plik.write(inputString.toString());
-			
+			int indek = inputString.indexOf(",\"doc\":");
+			inputString = inputString.substring(indek+7);
+			inputString =  inputString.substring(0, inputString.length()-2)+",";
+			plik.write(inputString.toString()+"\n");
+			i++;
+			System.out.println(i);
+			inputString2=inputString;
 		}
+		plik.write(inputString2);
 		response.close();
 		plik.close();
 		

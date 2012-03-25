7ae539164175a1af7c6e4d5cc8ba3ef6fefd57d7
Daniel <majin.d@gmail.com>
2012-03-25 18:25:47 +0200

    odczyt 4 baz couch zrobiony do { }

--- Odczyt.java
+++ Odczyt.java
@@ -5,36 +5,38 @@ public class Odczyt {
 
 	public static void main(String[] args) throws IOException {
 		
-		czytaj();
+		czytaj("katastrofy");
+		czytaj("tatry");
+		czytaj("meteo");
+		czytaj("pojufo");
 
 	}
 	
-	public static void czytaj() throws IOException{
-		FileWriter plik = new FileWriter("httpresponse1.txt");
+	public static void czytaj(String nazwa) throws IOException{
+		FileWriter plik = new FileWriter(nazwa+".txt");
 		//BufferedWriter bufor = new BufferedWriter(plik);
 		
-		URL link = new URL("http://localhost:5984/katastrofy/_all_docs?keys=[%22ID1%22,%22ID2%22]&include_docs=true");
+		URL link = new URL("http://localhost:5984/"+nazwa+"/_all_docs?keys=[%22ID1%22,%22ID2%22]&include_docs=true");
 		URLConnection polaczenie = link.openConnection();
 		BufferedReader response = new BufferedReader(new InputStreamReader(polaczenie.getInputStream()));
 		String inputString = "";
-		String inputString2 = "";
+		//String inputString2 = "";
 		int i = 0;
 		response.readLine();
 		while ((inputString = response.readLine())!=null) {
+			if(inputString.length()>7){
 			int indek = inputString.indexOf(",\"doc\":");
+			
 			inputString = inputString.substring(indek+7);
 			inputString =  inputString.substring(0, inputString.length()-2)+",";
 			plik.write(inputString.toString()+"\n");
 			i++;
 			System.out.println(i);
-			inputString2=inputString;
+			}
 		}
-		plik.write(inputString2);
 		response.close();
 		plik.close();
 		
-		//bufor.flush();
-		//bufor.close();
 	} 
 
 }

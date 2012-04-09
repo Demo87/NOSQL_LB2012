ad9db7e29732c350382ab04902b28776815d39c8
Daniel <majin.d@gmail.com>
2012-04-10 01:10:58 +0200

    wprowadzanie do mongo - ok

--- Wczytywanie.java
+++ Wczytywanie.java
@@ -9,24 +9,23 @@ import java.util.Set;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.DBDecoder;
-import com.mongodb.DBEncoder;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
-import com.mongodb.ReadPreference;
-import com.mongodb.WriteConcern;
-import com.mongodb.WriteResult;
 
+import com.mongodb.util.JSON;
 
 public class Wczytywanie {
 
 	public static void main(String[] args) throws MongoException, IOException {
 		wstaw("tatry");
+		wstaw("meteo");
+		wstaw("pojufo");
+		wstaw("katastrofy");
 
 	}
-	
-	public static void wstaw(String nazwa) throws MongoException, IOException{
+
+	public static void wstaw(String nazwa) throws MongoException, IOException {
 		Mongo mon = new Mongo();
 		DB baza = mon.getDB(nazwa);
 		System.out.println(baza.getStats());
@@ -34,28 +33,38 @@ public class Wczytywanie {
 		System.out.println(mon.getConnectPoint());
 		Set<String> kolekcje = baza.getCollectionNames();
 		System.out.println("Kolekcje");
-		for(String s: kolekcje){
+		for (String s : kolekcje) {
 			System.out.println(s);
 		}
 		System.out.println("Koniec kolekcji");
 		DBCollection kolekcja = baza.getCollection(nazwa);
-		BufferedReader plik = new BufferedReader(new FileReader(nazwa+".txt"));
+		BufferedReader plik = new BufferedReader(new FileReader(nazwa + ".txt"));
 		String linia = "";
-		int i =0;
-		while(((linia=plik.readLine())!=null)&& i<1){
+		int i = 0;
+		while ((linia = plik.readLine()) != null) {
 			i++;
-			linia = linia.substring(1);
-			System.out.println(i+" tu jest linia "+linia);			
-			String lista[] = linia.split("[{}]");
-			System.out.println(lista[0]);
-			System.out.println(lista[1]);
-/*			String lista[] = linia.split(",\"");
-			lista[0]=lista[0].replace("{\"","");
-			for (int j = 0; j < lista.length; j++) {
-				String element = lista[j];
-				String tmp[] = element.split(":");
-				System.out.println(tmp[0]+" a teraz wartość  "+tmp[1]);
-			}*/
+			// linia=linia.substring(1);
+			linia = linia.substring(0, linia.lastIndexOf(","));
+			if(linia.endsWith("}")){
+				
+			}
+			else{
+				linia = linia + "}";
+			}
+			// System.out.println(i+" tu jest linia "+linia);
+			String lista[] = linia.split(",");
+			// System.out.println(lista[0]);
+			// System.out.println(lista[1]);
+			linia = "{"
+					+ linia.replace(lista[0] + ",", "").replace(lista[1] + ",",
+							"");
+			// System.out.println(i+" tu jest linia "+linia);
+			//linia = linia.replaceAll("\"", "\'");
+			//System.out.println(i + " tu jest linia " + linia);
+			System.out.println(nazwa+" "+i);
+			DBObject obiekt = (DBObject) JSON.parse(linia);
+			//System.out.println(obiekt.get("id"));
+			kolekcja.insert(obiekt);
 
 		}
 	}

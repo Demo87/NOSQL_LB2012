77a4ef8b776beae899eab913dbed613e0eb2fbd2
Daniel <majin.d@gmail.com>
2012-05-21 18:50:02 +0200

    Porpawki v2

--- NOsql.java
+++ NOsql.java
@@ -10,6 +10,9 @@ public class NOsql {
 		for(int i=0;i<args.length;i++){
 			if(args[i].contains("db=")){
 				nazwa=args[i].replaceAll("db=", "");
+				if(nazwa.contains(".txt")){
+					nazwa=nazwa.replace(".txt", "");
+				}
 				
 			}
 		}
